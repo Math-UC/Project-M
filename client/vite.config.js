a681f5eef188a3aad5ad89f095e30e0558d07d73
tz <tonyzh21@gmail.com>
2025-04-12 22:53:36 -0400

    beginning of styling :)

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       src: path.resolve(__dirname, 'src'),
       pages: path.resolve(__dirname, 'src/pages'),
       components: path.resolve(__dirname, 'src/components'),
       utils: path.resolve(__dirname, 'src/utils'),
+      // assets: path.resolve(__dirname, 'src/assets'),
       // Add more aliases as needed
     },
   },
